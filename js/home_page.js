2d29e9a467ed84cd18ee51b2ecccd38da1e62d54
anupama cv <anu.anupamacv@gmail.com>
2021-07-13 15:45:42 +0530

    Modified: by adding functionality to display all employee payroll details from JSON object

--- js/home_page.js
+++ js/home_page.js
@@ -5,14 +5,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => { 
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                        "<th>Salary</th><th>Start Date</th><th>Actions</th>"
-    let empPayrollData = createEmployeePayrollJSON()[0];
-    const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
         <tr>
             <td><img class="profile" src="${empPayrollData._profilePic}" alt="">
             </td>
             <td>${empPayrollData._name}</td>
             <td>${empPayrollData._gender}</td>
-            <td><div class="dept-label">${empPayrollData._department[0]}</div><div class='dept-label' >${empPayrollData._department[1]}</div></td>
+            <td><${getDptHtml(empPayrollData._department)}</td>
             <td>${empPayrollData._salary}</td>
             <td>${empPayrollData._startDate}</td>
             <td>
@@ -21,6 +23,7 @@ const createInnerHtml = () => {
             </td>
         </tr> 
     `;
+    }   
     document.querySelector('#table-display').innerHTML = innerHtml;
 };
 
