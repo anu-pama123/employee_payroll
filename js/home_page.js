53b32aef5b5b3266277d9e443185538143375cb5
anupama cv <anu.anupamacv@gmail.com>
2021-07-13 15:13:38 +0530

    Added: functionality to display employee details from JSON object

--- js/home_page.js
+++ js/home_page.js
@@ -5,21 +5,54 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => { 
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                        "<th>Salary</th><th>Start Date</th><th>Actions</th>"
+    let empPayrollData = createEmployeePayrollJSON()[0];
     const innerHtml = `${headerHtml}
         <tr>
-            <td><img class="profile" src="../assets/profile-images/Ellipse -2.png" alt="">
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt="">
             </td>
-            <td>Rayan</td>
-            <td>Male</td>
-            <td><div class="dept-label">HR</div><div class='dept-label' >Finance</div></td>
-            <td>300000</td>
-            <td>1 Nov 2020</td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td><div class="dept-label">${empPayrollData._department[0]}</div><div class='dept-label' >${empPayrollData._department[1]}</div></td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
             <td>
-                <img name="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
-                <img name="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
+                <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
+                <img name="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
             </td>
         </tr> 
     `;
     document.querySelector('#table-display').innerHTML = innerHtml;
 };
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Rayan',
+            _gender: 'Male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '20 oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+        },
+        {
+            _name: 'Anupama keerthi shashanka',
+            _gender: 'Female',
+            _department: [
+                'Sales'
+            ],
+            _salary: '400000',
+            _startDate: '29 oct 2019',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
+
+        }
+    ];
+    return empPayrollListLocal;
+}
     
